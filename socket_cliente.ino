72c08b5b09064101ca3576e5e5ff3a70ef682caf
amandargusmao <104590590+amandargusmao@users.noreply.github.com>
2023-10-21 00:28:39 -0300

    Add files via upload

---
+++ socket_cliente.ino
@@ -0,0 +1,55 @@
+#include <WiFi.h>
+
+const char* rede = "IFAL-Informatica";
+const char* senha = "somostodosifal";
+
+const uint16_t porta = 3000; 
+const char* servidor = "10.0.0.104"; 
+
+const int ledServidor = 2; 
+const int alarme = 23;
+const int ledGas = 32;
+const int sensor = 34; 
+const int vazamento = 1000;
+
+unsigned long int last_time = millis();
+
+void setup() {
+pinMode(ledServidor, OUTPUT); 
+pinMode(ledGas, OUTPUT);
+pinMode(alarme, OUTPUT);
+pinMode(sensor, INPUT); 
+
+WiFi.begin(rede, senha); 
+while (WiFi.status() != WL_CONNECTED) {
+delay(80);
+}
+}
+
+void loop() {
+int leituraSensor = analogRead(sensor); 
+
+if (leituraSensor >= (vazamento)){
+  digitalWrite(alarme, HIGH);
+  digitalWrite(ledGas, HIGH);
+} else {
+  digitalWrite(alarme, LOW);
+  digitalWrite(ledGas, LOW);
+}
+
+WiFiClient client;
+if (!client.connect(servidor, porta)) {
+digitalWrite(ledServidor, LOW);
+return;
+}
+
+digitalWrite(ledServidor, HIGH); 
+client.println(leituraSensor); 
+
+if (millis() - last_time >= 5000){
+  last_time = millis();
+}
+
+last_time = millis();
+delay(1500);
+}
